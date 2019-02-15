6fb858a012592c2035a57c4b366647755cb37896
khenryjoe <joseph.ketterer@gmail.com>
2019-02-15 15:25:21 +0000

    pug templates

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const path = require('path');
 const rootDir = require('./util/path')
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 const port = 5000;
 
@@ -15,12 +18,12 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(express.static(path.join(rootDir, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res) =>{
    
-    res.status(404).sendFile(path.join(rootDir,  'views', '404.html'))
+    res.status(404).render('404', {docTitle: 'Page not found', pageTitle: 'Page not found' })
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 const rootDir = require('../util/path')
 const router = express.Router();
+const adminData = require('./admin');
 
 router.get('/', (req, res)=>{
-    console.log('hello');
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'Shop', pageTitle: 'Shop',  path: '/'});
 });
 
 module.exports = router;
