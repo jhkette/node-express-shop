3389f11d730573875653f5633df26de6e4eb1626
khenryjoe <joseph.ketterer@gmail.com>
2019-02-17 20:20:44 +0000

    commit

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const path = require('path');
-const rootDir = require('./util/path');
-const errorController = require('./controllers/error')
+
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const port = 5000;
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
-
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-
-
-
-
+app.listen(3000);
