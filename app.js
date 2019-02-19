c096e2671108ebced86088bf488523d1926ac3fb
khenryjoe <joseph.ketterer@gmail.com>
2019-02-19 20:40:16 +0000

    halfway through sequelize version

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user')
 
 const app = express();
 
@@ -18,9 +20,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next)=>{
+    User.findById(1)
+    .then(user =>{
+        req.user = user;
+        next();
+    })
+    .catch(err=> console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+
+sequelize.sync()
+.then(result =>{
+   return User.findByPk(1); 
+})
+.then(user =>{
+    if(!user){
+        return User.create({name: 'Joe', email: 'test@gmail.com'})
+    }
+    return Promise.resolve(user);
+})
+.then(user =>{
+   
+})
+.catch(err => {
+    console.log(err);
+})
 
 app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Gue55wh0s1n'
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('node-complete', 'root', 'Gue55wh0s1n', {
+  host: 'localhost',
+  dialect: 'mysql'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
